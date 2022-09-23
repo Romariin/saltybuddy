0ec18bae63490d07801f8c288c17be9a8e2f40fe
Romariin <romainrogier2@gmail.com>
2022-09-24 01:32:12 +0200

    💩 parseMatch (not all the regex working)

--- bot/classes/twitch.ts
+++ bot/classes/twitch.ts
@@ -2,26 +2,38 @@ import tmi from 'tmi.js';
 import * as color from 'colorette';
 
 class Twitch {
-    static async connectTwitch(username:string, password:any): Promise<any> {
-        console.log(color.bgBlue(' [Auth] '), color.blue(`🌎 Starting auth..` ));
+    static async connectTwitch(channel:any): Promise<any> {
+        console.log(color.bgBlue(' [Auth] '), color.blue(`🌎 Starting auth process..` ));
         const client = new tmi.client({
-            identity: {
-                username: username,
-                password: password
-            },
             connection: {
                 secure: true,
                 reconnect: true
             },
-            channels: ['saltybet']
+            channels: [channel]
         });
         client.connect().then(() => {
-            console.log(color.bgGreenBright(' [Auth] '), color.green(`✅ Authenticated ${color.bold(username)}` ));
+            console.log(color.bgGreenBright(' [Auth] '), color.green(`✅ Authenticated` ));
         }).catch((err:any) => {
-            console.log(color.bgRed(' [Auth] '), color.red(`🌎 Authentication failed ${color.bold(username)}` ));
+            console.log(color.bgRed(' [Auth] '), color.red(`🌎 Authentication failed` ));
             console.log(err);
         });
         return client;
+    }
+
+    static async parseMatch(str:any): Promise<any> {
+        switch(str) {
+            case (str.match(/Bets are OPEN for (.*) vs (.*)!/) || {}).input:
+                const matchData = str.match(/Bets are OPEN for (.*) vs (.*)!/);
+                console.log(color.bgYellowBright(' [Game] '), color.yellow(`🔥 Match\n└`), color.redBright(matchData[1]), color.white('vs'), color.blueBright(matchData[2]));
+                break;
+            case (str.match(/Bets are (.*)./) || {}).input:
+                const matchState = str.match(/Bets are (.*).!/);
+                console.log(color.bgYellowBright(' [Game] '), color.yellow(`🔥 Match\n└`), color.redBright(`State is now: ${color.bold(matchState)}`));
+                break;
+            case (str.match(/Payouts to Team (.*)./) || {}).input:
+                console.log('Payouts', str);
+                break;
+        }
     }
 }
 
